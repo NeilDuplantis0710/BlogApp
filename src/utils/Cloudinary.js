0eb011140e37d3fe0e78c595d969738dd8eb70e4
ishan <ishansinha2008@gmail.com>
2026-08-31 00:31:23 +0530

    feat: implement Cloudinary upload utility and configure environment variables

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -1,1 +1,28 @@
-import {v2} from "cloudinary"
+import {v2 as cloudinary} from "cloudinary"
+import { response } from "express"
+import fs from 'fs'
+import { loadEnvFile } from "process"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+
+const uploadOnCloudinary = async(localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        // uploading the file on cloudinary
+        cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto" // Whatever type of file you wanna upload you can. Figure it out yourself
+        })
+        console.log("File is not uploaded successfully on cloudinary", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved termporary file as the upload operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
