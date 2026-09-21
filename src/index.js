044215d2df12afa38060992d0e36d38af807ec0b
ishan <ishansinha2008@gmail.com>
2026-09-21 18:56:49 +0530

    feat: add Redis connection to server startup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
 import { app } from './app.js'
+import { connectRedis } from './redis/index.js'
 
 dotenv.config({ path: './.env' })
 
 const startServer = async () => {
     try {
         await connectDB()
+        await connectRedis()
         app.listen(process.env.PORT || 5000, () => {
             console.log(`The server is up and running at port ${process.env.PORT || 5000}`)
         })
