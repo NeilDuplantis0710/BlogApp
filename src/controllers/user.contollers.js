f8aeb4056660a3af9fd6caf8ad66d27ad1418d81
ishan <ishansinha2008@gmail.com>
2026-09-24 17:24:50 +0530

    feat: implement writePost function for creating blog posts with validation

--- src/controllers/user.contollers.js
+++ src/controllers/user.contollers.js
@@ -2,6 +2,8 @@ import { asyncHandler } from "../utils/AyncHandler.js"
 import { ApiError } from "../utils/ApiError.js"
 import { apiResponse } from "../utils/ApiResponse.js"
 import { User } from "../models/user.models.js"
+import { Post } from "../models/post.models.js"
+import { application } from "express"
 
 
 
@@ -58,9 +60,55 @@ const userSignUp = asyncHandler(async (req, res) => {
 
 const getAllUsers = asyncHandler(async (req,res) => {
     const users = await User.find({})
-    return res.status(200).json(new apiResponse(201, users, "Users fetched successfully!!"))
+    return res.status(201).json(new apiResponse(201, users, "Users fetched successfully!!"))
 })
-export {userSignUp, getAllUsers}
 
 
 // Blog Post
+const writePost = asyncHandler(async(req,res) => {
+    const { postName, postAuthor, postSubContent, postTag, postContent} = req.body()
+
+    console.log("Post - Name: ", postName)
+    console.log("Post - Author: ", postAuthor)
+    console.log("Post - Subcontent: ", postSubContent)
+    console.log("Post - Tag: ", postTag)
+    console.log("Post - Content: ", postContent)
+
+    if(!req.body() || req.body() == ''){
+        throw new ApiError(400, "Request Body not found!!")
+    }
+
+    if(!postName || postName == ''){
+        throw new ApiError(400, "Post Name is required!!!")
+    }
+
+    if(!postAuthor || postAuthor == ''){
+        throw new ApiError(400, "Post Author is required!!!!")
+    }
+
+    if(!postTag || postTag == ''){
+        throw new ApiError(400, 'Post Tag is required!!!')
+    }
+
+    if(!postContent || postContent == ''){
+        throw new ApiError(400, 'Post Content is required!!!')
+    }
+
+    const post = await Post.create({
+        postName: postName.trim().toLowercase(),
+        postAuthor: postAuthor.trim().toLowercase(),
+        postContent: postContent.toLowercase(),
+        postSubContent: postSubContent.toLowercase(),
+        postTag: postTag.trim().toUpperCase(),
+    }) 
+
+    const createdPost = await Post.findById(post._id)
+
+    if(!createdPost){
+        throw new ApiError(500, "The post can't be created!!!")
+    }
+
+    return res.status(201).json(new apiResponse(201, "Post created Successfully!!!", createdPost))
+})
+
+export {userSignUp, getAllUsers, writePost}
