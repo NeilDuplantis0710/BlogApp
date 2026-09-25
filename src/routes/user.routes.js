84c99db69912100dfff36e5484ded54dac81be25
ishan <ishansinha2008@gmail.com>
2026-09-25 14:35:28 +0530

    feat: add getAllBlogs route to retrieve all blog posts

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { userSignUp, getAllUsers, writePost } from "../controllers/user.contollers.js"
+import { userSignUp, getAllUsers, writePost, getAllBlogs } from "../controllers/user.contollers.js"
 
 const router = Router() //Creating a router object.
 
 router.route("/register").post(userSignUp)
 router.route("/getUsers").get(getAllUsers)
 router.route("/writePost").post(writePost)
+router.route("/getPosts").get(getAllBlogs)
 
 export default router
